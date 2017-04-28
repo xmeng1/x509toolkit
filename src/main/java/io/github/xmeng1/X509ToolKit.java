adc1860b54fb281aea59609468d96bb831e7b85f
Xin Meng <x.meng@outlook.com>
2017-04-29 00:22:41 +0100

    add maven dependencies
    1. spock
    2. bouncy castle
    3. add groovy library, use SdkMan install groovy in .sdkman/candidates/groovy/

---
+++ src/main/java/io/github/xmeng1/X509ToolKit.java
@@ -0,0 +1,10 @@
+package io.github.xmeng1;
+
+/**
+ * User:    Xin Meng
+ * Date:    29/04/17
+ * Project: x509toolkit
+ */
+public class X509ToolKit {
+
+}
---
+++ src/test/groovy/io/github/xmeng1/X509ToolKitSpec.groovy
@@ -0,0 +1,13 @@
+package io.github.xmeng1
+
+import spock.lang.Specification
+
+
+/**
+ * User:    Xin Meng
+ * Date:    29/04/17
+ * Project: Default (Template) Project
+ */
+class X509ToolKitSpec extends Specification {
+
+}
